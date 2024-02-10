ab8e3375dc70c396bfb617a48487a2c3cddf43c3
Benedetto Marco Serinelli <benedettomarco.serinelli@gmail.com>
2024-02-10 15:50:17 +0100

    feat: nav bar supports i18n

--- kafka-ui-react-app/src/components/NavBar/NavBar.tsx
+++ kafka-ui-react-app/src/components/NavBar/NavBar.tsx
@@ -1,87 +1,102 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Select from 'components/common/Select/Select';
 import Logo from 'components/common/Logo/Logo';
 import Version from 'components/Version/Version';
-import GitIcon from 'components/common/Icons/GitIcon';
-import DiscordIcon from 'components/common/Icons/DiscordIcon';
 import AutoIcon from 'components/common/Icons/AutoIcon';
 import SunIcon from 'components/common/Icons/SunIcon';
 import MoonIcon from 'components/common/Icons/MoonIcon';
 import { ThemeModeContext } from 'components/contexts/ThemeModeContext';
-
 import UserInfo from './UserInfo/UserInfo';
 import * as S from './NavBar.styled';
-
+import i18n from 'i18next';
+import GitIcon from '../common/Icons/GitIcon';
+import DiscordIcon from '../common/Icons/DiscordIcon';
 interface Props {
   onBurgerClick: () => void;
 }
 
-export type ThemeDropDownValue = 'auto_theme' | 'light_theme' | 'dark_theme';
-
-const options = [
-  {
-    label: (
-      <>
-        <AutoIcon />
-        <div>Auto theme</div>
-      </>
-    ),
-    value: 'auto_theme',
-  },
-  {
-    label: (
-      <>
-        <SunIcon />
-        <div>Light theme</div>
-      </>
-    ),
-    value: 'light_theme',
-  },
-  {
-    label: (
-      <>
-        <MoonIcon />
-        <div>Dark theme</div>
-      </>
-    ),
-    value: 'dark_theme',
-  },
-];
-
 const NavBar: React.FC<Props> = ({ onBurgerClick }) => {
+  const { t } = useTranslation(); // Initialize useTranslation hook
   const { themeMode, setThemeMode } = useContext(ThemeModeContext);
+  const languageOptions: OptionTypeBase[] = [
+    { label: 'en', value: 'en' },
+    { label: 'it', value: 'it' },
+  ];
+
+  // Function to change the language
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng); // Change the language
+  };
+
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const options = [
+    {
+      label: (
+        <>
+          <AutoIcon />
+          <div>{t('autoTheme')}</div>
+        </>
+      ),
+      value: 'auto_theme',
+    },
+    {
+      label: (
+        <>
+          <SunIcon />
+          <div>{t('lightTheme')}</div>
+        </>
+      ),
+      value: 'light_theme',
+    },
+    {
+      label: (
+        <>
+          <MoonIcon />
+          <div>{t('darkTheme')}</div>
+        </>
+      ),
+      value: 'dark_theme',
+    },
+  ];
 
   return (
     <S.Navbar role="navigation" aria-label="Page Header">
       <S.NavbarBrand>
-        <S.NavbarBrand>
-          <S.NavbarBurger
-            onClick={onBurgerClick}
-            onKeyDown={onBurgerClick}
-            role="button"
-            tabIndex={0}
-            aria-label="burger"
-          >
-            <S.Span role="separator" />
-            <S.Span role="separator" />
-            <S.Span role="separator" />
-          </S.NavbarBurger>
+        <S.NavbarBurger
+          onClick={onBurgerClick}
+          onKeyDown={onBurgerClick}
+          role="button"
+          tabIndex={0}
+          aria-label="burger"
+        >
+          <S.Span role="separator" />
+          <S.Span role="separator" />
+          <S.Span role="separator" />
+        </S.NavbarBurger>
 
-          <S.Hyperlink to="/">
-            <Logo />
-            UI for Apache Kafka
-          </S.Hyperlink>
+        <S.Hyperlink to="/">
+          <Logo />
+          UI for Apache Kafka
+        </S.Hyperlink>
 
-          <S.NavbarItem>
-            <Version />
-          </S.NavbarItem>
-        </S.NavbarBrand>
+        <S.NavbarItem>
+          <Version />
+        </S.NavbarItem>
       </S.NavbarBrand>
+
       <S.NavbarSocial>
         <Select
           options={options}
           value={themeMode}
           onChange={setThemeMode}
+          isThemeMode
+        />
+        <Select
+          options={languageOptions}
+          value={selectedLanguage}
+          onChange={(value: string) => changeLanguage(value)}
+          placeholder={i18n.language}
           isThemeMode
         />
         <S.SocialLink
